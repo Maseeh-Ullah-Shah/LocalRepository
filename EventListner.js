ad77b578c683f2185973d88e91f83b1193064780
Maseeh-Ullah-Shah <maseehullahshah13@gmail.com>
2026-02-17 16:12:09 +0500

    Form practice

--- EventListner.js
+++ EventListner.js
@@ -9,9 +9,31 @@ const para = document.querySelector("p");
 //     console.log("single Clicked!!");
 // })
 
- userNameInput.addEventListener("input",(e)=>{
-    console.log(e.target.value);
-    para.textContent = e.target.value;
+//  userNameInput.addEventListener("input",(e)=>{
+//     console.log(e.target.value);
+//     para.textContent = e.target.value;
+//     console.log("input events fires");
+// })
+
+
+//It works only when we change the value inside input field and leave outside the field
+// userNameInput.addEventListener("change",(e)=>{
+//     console.log(e.target.value);
+//     para.textContent = e.target.value;
+//     console.log("input events fires");
+// })
+
+//It works only when we type inside the field
+
+// userNameInput.addEventListener("focus",(e)=>{
+//     console.log(e.target.value);
+//     para.textContent = e.target.value;
+//     console.log("input events fires");
+// })
+
+//It works only when we click outside the input field
+userNameInput.addEventListener("blur",(e)=>{
+    console.log(e.type);
     console.log("input events fires");
 })
 
