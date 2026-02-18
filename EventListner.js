f4be52f177cae7398a041bf0bbbb91542d49886c
Maseeh-Ullah-Shah <maseehullahshah13@gmail.com>
2026-02-18 08:53:30 +0500

    Event Bubbling and Capturing Practice

--- EventListner.js
+++ EventListner.js
@@ -1,39 +1,35 @@
-const userNameInput = document.querySelector("#userName");
-const para = document.querySelector("p");
+const green = document.querySelector(".green");
+const pink = document.querySelector(".pink");
+const blue = document.querySelector(".blue");
 
-// userNameInput.addEventListener("dblclick",()=>{
-//     console.log("Double Clicked!!");
-// })
+// green.addEventListener("click",(e)=>{
+//    console.log("3.Green event Listner")
+// },true)
 
-// userNameInput.addEventListener("click",()=>{
-//     console.log("single Clicked!!");
-// })
+green.addEventListener("click",(e)=>{
+   console.log("3.Green event Listner");
+   e.stopPropagation();
+},{once:true});
 
-//  userNameInput.addEventListener("input",(e)=>{
-//     console.log(e.target.value);
-//     para.textContent = e.target.value;
-//     console.log("input events fires");
-// })
+pink.addEventListener("click",(e)=>{
+   console.log("2.pink event Listner")
+},true)
 
+blue.addEventListener("click",(e)=>{
+   console.log("1. Blue event Listner")
+},true)
+document.body.addEventListener("click",()=>{
+   console.log("4. body is clicked!!");
+},true)
 
-//It works only when we change the value inside input field and leave outside the field
-// userNameInput.addEventListener("change",(e)=>{
-//     console.log(e.target.value);
-//     para.textContent = e.target.value;
-//     console.log("input events fires");
-// })
+document.documentElement.addEventListener("click",()=>{
+   console.log("5. Document element is clicked!!");
+},true)
 
-//It works only when we type inside the field
+document.addEventListener("click",()=>{
+   console.log("6. document is clicked!!");
+},true)
 
-// userNameInput.addEventListener("focus",(e)=>{
-//     console.log(e.target.value);
-//     para.textContent = e.target.value;
-//     console.log("input events fires");
-// })
-
-//It works only when we click outside the input field
-userNameInput.addEventListener("blur",(e)=>{
-    console.log(e.type);
-    console.log("input events fires");
-})
-
+window.addEventListener("click",()=>{
+   console.log("7. window is clicked!!");
+},true)
